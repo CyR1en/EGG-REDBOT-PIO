e9cdb31164fc342828ba46112c34e0f9da3ba1ce
CyR1en <cyrien3519@gmail.com>
2019-10-11 00:06:25 -0600

    Use Tasks

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,22 @@
 #include <BikeBot.h>
+#include <Tasks.h>
 
-BikeBot bikeBot;
+BikeBot *bikeBot;
+Tasks *tasks;
 
 int buttonPin = 12;
 
 void setup(){
+    BikeBot b;
+    bikeBot = &b;
+
+    tasks = Task(*bikeBot).construct<SRS>();
     pinMode(buttonPin, INPUT_PULLUP);
     Serial.begin(9600);
 }
 
 void loop(void){
     if (digitalRead(buttonPin) == LOW){
-        bikeBot.pivotPrecise(90);
+        tasks->executeTask();
     }
 }
